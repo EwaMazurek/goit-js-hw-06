dc7ecd3ee65ec80e83e4d7273632fe84861f21c2
EwaMazurek <ewa.mmazurek@gmail.com>
2023-01-18 11:54:39 +0100

    slider initial value set to 0

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
 const slider = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-
+slider.value = 0;
 slider.addEventListener("input", event => {
   const fontSizeString = event.currentTarget.value + "px";
   text.style.fontSize = fontSizeString;
